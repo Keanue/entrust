1a3d94bd450189a0fa8173155db1c5bb559a9b42
Chevrier Quentin <quentin.chevrier@appiway.com>
2016-05-23 11:54:20 +0200

    custom user model - guard

--- src/Entrust/Middleware/EntrustPermission.php
+++ src/Entrust/Middleware/EntrustPermission.php
@@ -8,37 +8,39 @@
  * @package Zizaco\Entrust
  */
 
+use Auth;
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Support\Facades\Config;
 
 class EntrustPermission
 {
-	protected $auth;
+    protected $auth;
 
-	/**
-	 * Creates a new instance of the middleware.
-	 *
-	 * @param Guard $auth
-	 */
-	public function __construct(Guard $auth)
-	{
-		$this->auth = $auth;
-	}
+    /**
+     * Creates a new instance of the middleware.
+     *
+     * @param Guard $auth
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = Auth::guard(Config::get('entrust.guard'));
+    }
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  Closure $next
-	 * @param  $permissions
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next, $permissions)
-	{
-		if ($this->auth->guest() || !$request->user()->can(explode('|', $permissions))) {
-			abort(403);
-		}
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  Closure $next
+     * @param  $permissions
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $permissions)
+    {
+        if ($this->auth->guest() || !$this->auth->user()->can(explode('|', $permissions))) {
+            abort(403);
+        }
 
-		return $next($request);
-	}
+        return $next($request);
+    }
 }
--- src/Entrust/Middleware/EntrustRole.php
+++ src/Entrust/Middleware/EntrustRole.php
@@ -8,37 +8,39 @@
  * @package Zizaco\Entrust
  */
 
+use Auth;
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Support\Facades\Config;
 
 class EntrustRole
 {
-	protected $auth;
+    protected $auth;
 
-	/**
-	 * Creates a new instance of the middleware.
-	 *
-	 * @param Guard $auth
-	 */
-	public function __construct(Guard $auth)
-	{
-		$this->auth = $auth;
-	}
+    /**
+     * Creates a new instance of the middleware.
+     *
+     * @param Guard $auth
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = Auth::guard(Config::get('entrust.guard'));
+    }
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  Closure $next
-	 * @param  $roles
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next, $roles)
-	{
-		if ($this->auth->guest() || !$request->user()->hasRole(explode('|', $roles))) {
-			abort(403);
-		}
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  Closure $next
+     * @param  $roles
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $roles)
+    {
+        if ($this->auth->guest() || !$this->auth->user()->hasRole(explode('|', $roles))) {
+            abort(403);
+        }
 
-		return $next($request);
-	}
+        return $next($request);
+    }
 }
